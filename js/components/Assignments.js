bd4cc5b046d6b1d06f65cbf54995dbab945eac1b
codeninjaug <segayiandrew@gmail.com>
2022-05-28 15:26:55 +0300

    pushing to an array and making a v-model for the value

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -3,7 +3,13 @@ export default{
     components:{ AssignmentList },
     template : 
     `<assignment-list :assignments="filters.inProgress" title="In Progress Tasks"></assignment-list>
-     <assignment-list :assignments="filters.completed" title="Completed Tasks"></assignment-list>`,
+     <assignment-list :assignments="filters.completed" title="Completed Tasks"></assignment-list> 
+     <form @submit.prevent="add">
+         <input v-model="newAssignment" placeholder="Enter Task">
+         <button type="submit">Add Task</button>
+     </form>
+     `,
+
  data() {
         return {
            assignments:[
@@ -11,14 +17,12 @@ export default{
            {name :"Read Chapter 4" , complete:false , id:2},
            {name :"Turn in HomeWork" , complete:false, id:3},
            {name :"Code project" , complete:false, id:4},
-        ]
+        ],
+         newAssignment:'',
            //greeting:'What is Up'
         };
     },
 computed:{
-   
-
-
     filters(){
         return {
           inProgress: this.assignments.filter(assignment => !assignment.complete),
@@ -32,6 +36,17 @@ computed:{
         //     return this.assignments.filter(assignment => ! assignment.complete);
         // }
 
+       },
+       methods:{
+          add(){
+            //   alert(this.newAssignment)
+            this.assignments.push({
+               name: this.newAssignment,
+               completed:false,
+               id:this.assignments.length + 1
+            });
+            this.newAssignment = ""
+          }
        },
     //    components:{
     //     'app-button': AppButton,
